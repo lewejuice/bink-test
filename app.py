57423e75bb0fe40f4b3bcdcb65aa22be2b8a2774
lewejuice <hamiltonlewis@hotmail.com>
2021-09-24 13:37:09 +0000

    completed task 1 - Current rent

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import itertools
+from datetime import datetime, date, timedelta
 from flask import Flask, render_template, flash, \
     redirect, session, url_for, request
 
@@ -9,16 +11,41 @@ app = Flask(__name__)
 # Takes user to the home-page
 @app.route('/home')
 def home():
-    csvfile = open('csv/test-dataset.csv','r', newline='')
-    obj = csv.reader(csvfile)
-    for row in obj:
-        print (row)
     return render_template("index.html")
 
+"""
+Current rent function which displays current
+rent data in order starting with the lowest
+and prints first five to the console
+"""
 @app.route('/current_rent')
 def current_rent():
-    # Current rent button which displays current rent data and prints first five to the console
-    return render_template("current-rent.html")
+    # csv file name
+    filename = 'csv/test-dataset.csv'
+    
+    # reading csv file
+    with open(filename, 'r') as csvfile:
+        # creating a data object
+        data = csv.reader(csvfile, delimiter = ',')
+
+        # Defines the fields
+        fields = next(data)
+
+        # Starting count variable
+        count = 0
+
+        # Retrieves the current rent, rounds it, turns it into a integer and then sorts it in order
+        sort = sorted(data, key=lambda x : int(float(x[-1])))
+        # For loop to iterate through each line
+        for eachline in sort:
+            # Makes sure the line is not a field and makesure it doesn't go over 5 lines
+            if eachline is not fields and count < 5:
+                count += 1
+                print(eachline)
+
+        return render_template("current-rent.html")
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('0.0.0.0'),
